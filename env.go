2c08babb76893af9010659de4f8c4c6a6110742d
Eric Larson <elarson@digitalocean.com>
2017-12-21 01:05:52 -0600

    add env.go for env helpers

---
+++ env.go
@@ -0,0 +1,39 @@
+package procs
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ParseEnv(environ []string) map[string]string {
+	env := make(map[string]string)
+	for _, e := range environ {
+		pair := strings.SplitN(e, "=", 2)
+		env[pair[0]] = pair[1]
+	}
+	return env
+}
+
+func Env(env map[string]string, useEnv bool) []string {
+	envlist := []string{}
+
+	// update our env by loading our env and overriding any values in
+	// the provided env.
+	if useEnv {
+		environ := ParseEnv(os.Environ())
+		for k, v := range env {
+			environ[k] = v
+		}
+		env = environ
+	}
+
+	for key, val := range env {
+		if key == "" {
+			continue
+		}
+		envlist = append(envlist, fmt.Sprintf("%s=%s", key, val))
+	}
+
+	return envlist
+}
